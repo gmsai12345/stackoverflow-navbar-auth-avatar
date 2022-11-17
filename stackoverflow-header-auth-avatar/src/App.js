39a1a7d2949cf62717d821c70417729106556971
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-11-17 07:42:52 +0530

    Add files via upload

---
+++ stackoverflow-header-auth-avatar/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header1 from "./header";
+//import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+//import Route1 from "../../Route1";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      <Header1 />
+    </div>
+  );
+}
+ReactDOM.render(<div>{App()}</div>, document.getElementById("root"));
+
+export default App;
---
+++ stackoverflow-header-auth-avatar/src/Avatar/Avatar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Avatar = ({ children }) => {
+  return (
+    <div>
+      {children}
+      </div>
+
+  );
+
+}
+export default Avatar;
