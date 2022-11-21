fd35e9f01593a2219c3dd843ab8f8e1a69337fa0
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-11-21 17:12:37 +0530

    Update Home.jsx

---
+++ stack-overflow-nav-auth-avatar-jsx-3/src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+//import Home from './Home/Home'
+//import Auth from './Auth/Auth'
+const Home = () => {
+  
+  return (
+    <div>
+      
+      </div>
+  )
+}
+export default Home;
