15a310376ee8c281097e7f5e7507553ffcc38341
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-11-21 17:13:16 +0530

    Update button.jsx

---
+++ stack-overflow-nav-auth-avatar-jsx-3/src/button/button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+//import Home from './Home/Home'
+//import Auth from './Auth/Auth'
+const button = () => {
+  
+  return (
+    <div>
+      
+      </div>
+  )
+}
+export default button;
